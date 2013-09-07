c6a612b47024c5c17a5af440c961018bb13c018d
Jeson <bitnick2002@gmail.com>
2013-09-07 18:08:20 +0800

    c

--- net/http/session/session.go
+++ net/http/session/session.go
@@ -9,15 +9,17 @@ import (
 	"time"
 )
 
-type S map[string]*Content
+type S struct {
+	sessionmap map[string]*Content
+}
 
 type Content struct {
 	ObjectId       string
 	LastAccessTime time.Time
 }
 
-func New(duration time.Duration) (session S) {
-	session = make(S)
+func New(duration time.Duration) (session *S) {
+	session = &S{make(map[string]*Content)}
 	ticker := time.NewTicker(duration)
 	go func() {
 		for {
@@ -27,31 +29,37 @@ func New(duration time.Duration) (session S) {
 	}()
 	return session
 }
+func (session *S) Get(key string) *Content {
+	return session.sessionmap[key]
+}
+func (session *S) Len() int {
+	return len(session.sessionmap)
+}
 
-func (session S) Sweep(duration time.Duration) {
-	for key, value := range session {
+func (session *S) Sweep(duration time.Duration) {
+	for key, value := range session.sessionmap {
 		if value.LastAccessTime.Add(duration).Before(time.Now()) {
-			delete(session, key)
+			delete(session.sessionmap, key)
 		}
 	}
 }
-func (session S) Update(sessionId string) {
-	if session[sessionId] != nil {
-		session[sessionId].LastAccessTime = time.Now()
+func (session *S) Update(sessionId string) {
+	if session.sessionmap[sessionId] != nil {
+		session.sessionmap[sessionId].LastAccessTime = time.Now()
 	}
 
 }
-func (session S) Add(objectId string) (id string) {
+func (session *S) Add(objectId string) (id string) {
 	h := md5.New()
 	io.WriteString(h, objectId)
 	io.WriteString(h, randString())
 	id = hex.EncodeToString(h.Sum(nil))
 
-	for session[id] != nil {
+	for session.sessionmap[id] != nil {
 		io.WriteString(h, strconv.Itoa(rd()))
 		id = hex.EncodeToString(h.Sum(nil))
 	}
-	session[id] = &Content{objectId, time.Now()}
+	session.sessionmap[id] = &Content{objectId, time.Now()}
 	return id
 }
 func rd() int {
--- net/http/session/session_test.go
+++ net/http/session/session_test.go
@@ -17,19 +17,19 @@ func Test_(t *testing.T) {
 			session.Add("5216ea39d8b6d1a51d071d33")
 			session.Add("5216ea39d8b6d1a51d071d34")
 			id = session.Add("5216ea39d8b6d1a51d071d35")
-			Expect(len(session)).ToBe(5)
+			Expect(session.Len()).ToBe(5)
 		})
 		It("should update session", func() {
 			time.Sleep(duration / 2)
 			session.Update(id)
 			time.Sleep(duration - duration/10)
-			Expect(len(session)).ToBe(1)
-			Expect(session[id].ObjectId).ToBe("5216ea39d8b6d1a51d071d35")
+			Expect(session.Len()).ToBe(1)
+			Expect(session.Get(id).ObjectId).ToBe("5216ea39d8b6d1a51d071d35")
 		})
 		Describe("when session has been swept", func() {
 			It("should have no seesion", func() {
 				time.Sleep(duration)
-				Expect(len(session)).ToBe(0)
+				Expect(session.Len()).ToBe(0)
 			})
 		})
 	})
